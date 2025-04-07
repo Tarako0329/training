1710b13590c4f6e4cd9299507599625a76ffb2b5
Tarako0329 <86461908+Tarako0329@users.noreply.github.com>
2025-04-07 19:21:43 +0900

    ver1.20.6:記録時刻表示

--- header.php
+++ header.php
@@ -4,8 +4,8 @@
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
 <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="48x48" href="img/favicon.png">
-<!--<link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
-<link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#5bbad5">-->
+<link rel="icon" type="image/png" sizes="16x16" href="img/icon-16x16.png">
+<!--<link rel="mask-icon" href="img/safari-pinned-tab.svg" color="#5bbad5">-->
 <meta name="msapplication-TileColor" content="#2b5797">
 <meta name="theme-color" content="#ffffff">
 <!-- Bootstrap5 CSS -->
